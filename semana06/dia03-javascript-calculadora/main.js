578aae0cda5919bee0a5a4447e004bdece21a188
Carlos Branty <losbrantybc@yahoo.com>
2023-11-05 23:08:54 -0500

    Cambios en index y main

--- semana06/dia03-javascript-calculadora/main.js
+++ semana06/dia03-javascript-calculadora/main.js
@@ -1,1 +1,49 @@
-console.log('Hola como estas')
+
+
+
+let numeroActual = '0';
+let operador = '';
+let operando = '';
+
+const inputDisplay = document.querySelector('.inputDisplay')
+
+const buttons = document.querySelectorAll('.button')
+
+// console.log(imputDisplay)
+//  console.log(buttons)
+
+/* EVENTOS */
+
+buttons.forEach(function(button){
+
+    button.addEventListener('click', function(event){
+
+        const button = event.target
+        const buttonText = button.textContent
+
+        if('+-*'.includes(buttonText)){
+            operador = buttonText
+            operando = Number(numeroActual)
+            numeroActual = '0'
+        } else if(buttonText.trim() === '='){
+            if(operador === '+'){
+                numeroActual = Number(operando) + Number(numeroActual)
+            }
+            else if(operador === '-'){
+                numeroActual = Number(operando) - Number(numeroActual)
+            }
+            else if(operador === '*'){
+                numeroActual = Number(operando) * Number(numeroActual)
+            }
+        } else if(buttonText === 'AC'){
+            numeroActual = '0'
+            operador = ''
+            operando = ''
+        } else{
+            // se preciono alguno de los botones numericos 
+            // numeroActual=''
+            numeroActual= Number(numeroActual + buttonText)
+        }
+        inputDisplay.value=numeroActual;
+    })
+})
