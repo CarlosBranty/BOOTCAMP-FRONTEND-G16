5d98f3d0db8ec835dd54fd556d539cf78f931399
Carlos Branty <losbrantybc@yahoo.com>
2023-11-09 19:50:41 -0500

    Siguiendo

--- semana07/dia02-javascript-dom-todo-intermedio/main.js
+++ semana07/dia02-javascript-dom-todo-intermedio/main.js
@@ -1,1 +1,11 @@
-console.log("hola rintintin")
+
+const tasks = [] // aqui almacenaremos las tareas
+const taskInput = document.querySelector('.task__input');// selecciona el input desde javscript
+const taskList = document.querySelector('.task__list');//seleccion la clase desde javasctip
+
+taskInput.addEventListener('keypress', function(event){
+    // aqui ira la logica de nuestra aplicacion
+    if(event.key === "Enter"){
+        // console.log("Agregando una nueva tarea")
+    }
+})
