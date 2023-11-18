54ddb162aa6f8df45637be8027b2f8233ccab55a
Carlos Branty <losbrantybc@yahoo.com>
2023-11-17 20:53:22 -0500

    mas filtros

--- semana08/dia02-javascript-es6-fetch-coutries/modules/main.js
+++ semana08/dia02-javascript-es6-fetch-coutries/modules/main.js
@@ -1,12 +1,12 @@
 // console.log("hola")
 
-const url = 'https://restcountries.com/v3.1/all?fields=name,flags,region,capital,population,languages,currencies,timezones';
+const url =
+  "https://restcountries.com/v3.1/all?fields=name,flags,region,capital,population,languages,currencies,timezones";
 
-let coutryData = []
+let coutryData = [];
 
-const seachInput = document.querySelector('.app__input')
-const filterSelect = document.querySelector('.app__filter')
-
+const seachInput = document.querySelector(".app__input");
+const filterSelect = document.querySelector(".app__filter");
 
 // function fetchCoutries (){
 //     return fetch(url)
@@ -17,37 +17,56 @@ const filterSelect = document.querySelector('.app__filter')
 // fetchCoutries()
 // .then(data => console.log(data))
 
-seachInput.addEventListener('input', (event) => {
-    const value = event.target.value
-    // const value = seachInput.value
-    console.log(value)
-})
+seachInput.addEventListener("input", (event) => {
+  const value = event.target.value;
+  const loweredValue = value.toLowerCase();
 
+  const filteredCoutries = coutryData.filter((country) => {
+    const loweredName = country.name.common.toLowerCase();
+    
+    const joinedCapital = country.capital.join(',')
+    const loweredCapital = joinedCapital.toLowerCase() 
+    // TODO Adicionalmente necesitamos filtrar los paises por su capital
+
+
+    return loweredName.includes(loweredValue) || loweredCapital.includes(loweredValue);
+  });
+
+  renderCoutries(filteredCoutries);
+});
+
+filterSelect.addEventListener('input', (event) =>{
+  const value = event.target.value;
+  const filteredRegion = coutryData.filter((regionCoutry) =>{
+    const loweredRegion = regionCoutry.region.toLowerCase()
+    return loweredRegion.includes(value)
+  })
+  renderCoutries(filteredRegion)
+})
 
 // TODO: Usar async/await para la funcion fetchCoutries
 
-const fetchCoutriesAsync = async () =>{
-    try {
-        const respuesta = await fetch(url)
-        if (!respuesta.ok) {
-            throw new Error('ERROR:',respuesta.status)
-        }
-        const json = await respuesta.json()
-        return json
-        
-    } catch (error) {
-        console.log(error)
+const fetchCoutriesAsync = async () => {
+  try {
+    const respuesta = await fetch(url);
+    if (!respuesta.ok) {
+      throw new Error("ERROR:", respuesta.status);
     }
-}
+    const json = await respuesta.json();
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const renderCoutries = (coutries = []) =>{
-    // console.log(coutries)
-    const coutryListElement = document.querySelector('.app__list')
+const renderCoutries = (coutries = []) => {
+  // console.log(coutries)
+  const coutryListElement = document.querySelector(".app__list");
 
-    let coutryList =''
+  let coutryList = "";
 
-    coutries.forEach(country => {
-        coutryList += `
+  coutries.forEach((country) => {
+    coutryList += `
         <div class="country">
     <img class="country__flag" src="${country.flags.png}" alt="${country.flags.alt} />
     <div class="country__wrapper">
@@ -59,22 +78,21 @@ const renderCoutries = (coutries = []) =>{
         <strong>Region:</strong> ${country.region}
       </div>
       <div class="country__description">
-        <strong>Capital:</strong> ${country.capital}
+        <strong>Capital:</strong> ${country.capital[0]}
       </div>
     </div>
   </div>
         `;
-    })
-    coutryListElement.innerHTML = coutryList
-}
+  });
+  coutryListElement.innerHTML = coutryList;
+};
 
 // DOMContentLoaded -> Es un evento del navegador que espera a que el dom este cargado
 
-document.addEventListener('DOMContentLoaded', async () =>{
-
- const data = await fetchCoutriesAsync();
- coutryData = data;
- renderCoutries(data);
+document.addEventListener("DOMContentLoaded", async () => {
+  const data = await fetchCoutriesAsync();
+  coutryData = data;
+  renderCoutries(data);
 });
 
 // fetchCoutriesAsync()
