dcb677bccb6c00d54aa552996d51b92444d5857e
Carlos Branty <losbrantybc@yahoo.com>
2023-11-23 19:14:44 -0500

    Cambios y TODO

--- semana08/dia03-nodejs-crud/public/main.js
+++ semana08/dia03-nodejs-crud/public/main.js
@@ -1,25 +1,56 @@
-import { fetchPeliculas } from "./services.js";
-import { renderPeliculas } from "./peliculas.js";
+import { renderPeliculas } from "./peliculas.js"
+import { fetchPeliculas, createPelicula } from "./services.js"
 /***************************************************************/
-    /* EJEMPLO USANDO PROMESAS */
-    // document.addEventListener('DOMContentLoaded', (event) => {
-    //     console.log('DOM fully loaded and parsed');
-    //     fetchPeliculas()
-    //     .then(peliculas =>{
-    //         console.log(peliculas);
-    //         // Aqui rendereizaremos las peliculas
-    //     })
-    //   });
+/* EJEMPLO USANDO PROMESAS */
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     console.log('DOM fully loaded and parsed');
+//     fetchPeliculas()
+//     .then(peliculas =>{
+//         console.log(peliculas);
+//         // Aqui rendereizaremos las peliculas
+//     })
+//   });
+/****************************************************************/
+const peliculasForm = document.getElementById("peliculasForm");
 
+peliculasForm.addEventListener("submit", async (event) => {
+  event.preventDefault()
+
+  // console.log('Creando una nueva película...')
+
+  const peliculaForm = document.forms["peliculasForm"];
+
+  console.log(peliculaForm);
+
+  const nombre = peliculaForm.nombre.value;
+  const imagen = peliculaForm.imagen.value;
+  const estreno = peliculaForm.estreno.value;
+  const genero = peliculaForm.genero.value; // Este campo es del tipo cadena por defecto
+  const resumen = peliculaForm.resumen.value;
+
+  console.log({ nombre, imagen, estreno, genero, resumen });
+
+  const nuevaPelicula = {
+    nombre,
+    imagen,
+    estreno,
+    generoId: Number(genero),
+    resumen
+  };
+  console.log(nuevaPelicula);
+
+  const response = await createPelicula(nuevaPelicula);
+
+  
+  console.log(response)
+});
 /****************************************************************/
 
-  /* EJEMPLO USANDO ASYNC AWAIT */
-    document.addEventListener('DOMContentLoaded', async (event) => {
-
-        const peliculas = await fetchPeliculas();
-        console.log(peliculas);
-        renderPeliculas(peliculas);
-    });
+/* EJEMPLO USANDO ASYNC AWAIT */
+document.addEventListener("DOMContentLoaded", async (event) => {
+  const peliculas = await fetchPeliculas();
+  console.log(peliculas);
+    renderPeliculas(peliculas);
+});
 
 /*****************************************************************/
-
--- semana08/dia03-nodejs-crud/public/peliculas.js
+++ semana08/dia03-nodejs-crud/public/peliculas.js
@@ -1,35 +1,66 @@
-export const renderPeliculas = (peliculas) => {
-    const peliculasList = document.querySelector('.peliculas__list');
 
-    let elementos = '';
-    peliculas.forEach(pelicula =>{
-        elementos += `
+import { fetchPeliculas, deletePelicula } from "./services.js"
+
+
+/* TODOñ Terminar la edicion de peliculas usando el metodo PUT */
+
+export const renderPeliculas = (peliculas) => {
+    const peliculasList = document.querySelector('.peliculas__list')
+  
+    let elementos = ''
+  
+    peliculas.forEach(pelicula => {
+      elementos += `
         <tr>
-            <td>${pelicula.id}</td>
-            <td>
-                <img src="${pelicula.imagen}"
-                     width="100" height="250"/>
-            </td>
-            <td>
-                <strong>${pelicula.nombre}</strong>
-                <div class="fs-small">
-                    <strong>Estreno: </strong> ${pelicula.estreno}
-                </div>
-                <div class="fs-small">
-                    <strong>Genero: </strong> ${pelicula.generoId}
-                </div>
-                <div class="fs-small">
-                    <strong>Resumen: </strong> ${pelicula.resumen}
-                </div>
-            </td>
-            <td>
-            <div class="flex gap-0.5">
-                <button class="pelicula__edit">✏️</button>
-                <button class="pelicula__remove">❌</button>
+          <td>${pelicula.id}</td>
+          <td>
+            <img
+              src="${pelicula.imagen}"
+              onerror="this.src='https://placehold.co/300x450'"
+              width="100"
+              height="250"
+            />
+          </td>
+          <td>
+            <strong>${pelicula.nombre}</strong>
+            <div class="fs-small">
+              <strong>Estreno:</strong> ${pelicula.estreno}
             </div>
-            </td>
+            <div class="fs-small">
+              <strong>Genero:</strong> ${pelicula.genero.nombre}
+            </div>
+            <div class="fs-small">
+              <strong>Resumen:</strong> ${pelicula.resumen}
+            </div>
+          </td>
+          <td>
+            <div class="flex gap-0.5">
+              <button class="pelicula__edit">✏</button>
+              <button class="pelicula__remove" data-id="${pelicula.id}">❌</button>
+            </div>
+          </td>
         </tr>
-        `
+      `
     })
-    peliculasList.innerHTML = elementos;
-}
+  
+    peliculasList.innerHTML = elementos
+
+    const removeBotones = document.querySelectorAll('.pelicula__remove')
+    
+    removeBotones.forEach(button => {
+        button.addEventListener('click', async (event) => {
+          const id = event.target.dataset.id
+          console.log(id)
+          
+          const response = await deletePelicula(id)
+    
+          console.log(response)
+    
+          if (response) {
+            const peliculas = await fetchPeliculas()
+    
+            renderPeliculas(peliculas)
+          }
+        })
+      })
+  }
